f5eea6b2d9b61950336b2c7153bc15c11f3c52a6
Sch <junhg0211@gmail.com>
2021-07-26 15:18:13 +0900

    settings default

--- settings.py
+++ settings.py
@@ -1,7 +1,14 @@
 from json import load, dump
+from os.path import isfile
+from random import randint
 
-with open('./res/settings.json', 'r', encoding='utf-8') as file:
-    settings = load(file)
+_path = './res/settings.json'
+_default = {
+    "name": f"UNNAMED-{randint(0, 999999):06d}",
+    "room_title": "Room",
+    "port": "31872",
+    "host": "localhost"
+}
 
 
 def update(key, value):
@@ -13,3 +20,11 @@ def update(key, value):
 def save():
     with open('./res/settings.json', 'w', encoding='utf-8') as file_:
         dump(settings, file_)
+
+
+if isfile(_path):
+    with open(_path, 'r', encoding='utf-8') as file:
+        settings = load(file)
+else:
+    settings = _default.copy()
+    save()
